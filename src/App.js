e87f3a028786a20854af0e2ee3cda1aec624025f
Tehseen Afzal <abuashja@gmail.com>
2021-06-15 00:11:03 +0500

    Update App.js
    Render repo list in app

--- src/App.js
+++ src/App.js
@@ -2,20 +2,28 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  let [repos, setRepos] =useState([{}]);
-  useEffect(()=>{
-    async const getData = () =>{
-      const response = await fetch('https://api.github.com/users/muhammadmohsin/repos')
+  let [repos, setRepos] = useState([{}]);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(
+        "https://api.github.com/users/tahseenafzal/repos"
+      );
       const data = await response.json();
-      setRepos(data)
+      setRepos(data);
     }
     getData();
-  })
-  return (<div>
-    {repos.map((repoObj)=>{
-      <li key={ind}>{repoObj.name}</li>
-    })}
-  </div>);
+  }, []);
+  return (
+    <div>
+      <h1>You are seeing all repositories.</h1>
+      <ol>
+        {repos.map((repoObj, ind) => {
+          return <li key={ind}>{repoObj.name}</li>;
+        })}
+      </ol>
+    </div>
+  );
 }
 
 export default App;
