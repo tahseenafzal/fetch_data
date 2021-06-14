b8370fdfb57a511e6c2d0104991f43ff2f32237d
Tehseen Afzal <abuashja@gmail.com>
2021-06-14 23:47:36 +0500

    Update App.js
    Add useEffect, useState and get data using fetch api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let [repos, setRepos] =useState([{}]);
+  useEffect(()=>{
+    async const getData = () =>{
+      const response = await fetch('https://api.github.com/users/muhammadmohsin/repos')
+      const data = await response.json();
+      setRepos(data)
+    }
+    getData();
+  })
+  return (<div>
+    {repos.map((repoObj)=>{
+      <li key={ind}>{repoObj.name}</li>
+    })}
+  </div>);
 }
 
 export default App;
